478d5589c5edad2507ad9d7ec340d74d3f8119c9
AndreFSAntunes <andre.antunes@live.com>
2025-07-01 21:58:32 -0400

    writing ProductFactory

--- tests/factories.py
+++ tests/factories.py
@@ -20,7 +20,7 @@ Test Factory to make fake objects for testing
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
-
+from faker import Faker
 
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
@@ -32,3 +32,9 @@ class ProductFactory(factory.Factory):
 
     id = factory.Sequence(lambda n: n)
    ## Add code to create Fake Products 
+    name = factory.Faker("name")
+    description = factory.Faker('text')
+    price = fFuzzyDecimal(0.5, 2000.0, 2)
+    available = factory.Faker('boolean')
+    # i prefer this implementation over the suggested one
+    category = fuzzy.FuzzyChoice(list(Category))
